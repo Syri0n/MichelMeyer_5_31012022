26e1417676ed19a9748f2a3ea1840613dedad13c
Michel Meyer <syrion@outlook.com>
2022-02-02 14:42:45 +0100

    function getArticles

--- front/js/script.js
+++ front/js/script.js
@@ -4,3 +4,15 @@ fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
 .then((data) => getArticles(data))
 
+function getArticles(products) {
+    const imageUrl = products[0].imageUrl
+    
+    const items = document.querySelector("#items")
+  
+    const anchor = document.createElement("a")
+    anchor.href = imageUrl
+  
+    if (items != null) {
+      items.appendChild(anchor)
+    }
+  }
