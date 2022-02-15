261cf4ac235082f9e565683f310ffcee7c8b3f21
Michel Meyer <syrion@outlook.com>
2022-02-15 17:01:20 +0100

    add product.js & refacto

--- front/js/product.js
+++ front/js/product.js
@@ -1,56 +1,97 @@
-let queryString = window.location.search
-let urlParams = new URLSearchParams(queryString)
-let id = urlParams.get("id")
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let id = urlParams.get("id");
 
+if (id != null) {
+  let itemPrice = 0;
+  let imgUrl, altText;
+}
 
-fetch(`http://localhost:3000/api/products/${id}`)
-    .then((response) => response.json())
-    .then((res) => handleData(res))
+// On répupère l'id du produit depuis l'API
+
+fetch("http://localhost:3000/api/products/" + id)
+  .then((response) => response.json())
+  .then((res) => handleData(res));
 
 function handleData(kanap) {
-    // const altTxt = kanap.altTxt
-    // const colors = kanap.colors
-    // const description = kanap.description
-    // const imageUrl = kanap.imageUrl
-    // const name = kanap.name
-    // const price = kanap.price
-    // const _id = kanap._id
-
-    let { altTxt, colors, description, imageUrl, name, price, _id } = kanap;
-    makeImage(imageUrl, altTxt)
-    makeTitle(name)
-    makeDescription(description)
-    makePrice(price)
-    makeOptions(colors)
+  const { altTxt, colors, description, imageUrl, name, price } = kanap;
+  itemPrice = price;
+  imgUrl = imageUrl;
+  altText = altTxt;
+  makeImage(imageUrl, altTxt);
+  makeTitle(name);
+  makeDescription(description);
+  makePrice(price);
+  makeOptions(colors);
 }
 
 function makeImage(imageUrl, altTxt) {
-    let image = document.createElement("img");
-    image.src = imageUrl;
-    image.alt = altTxt;
-    let parent = document.querySelector(".item__img");
-    if (parent != null ) parent.appendChild (image);
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  const parent = document.querySelector(".item__img");
+  if (parent != null) parent.appendChild(image);
 }
 
 function makeTitle(name) {
-    let h1 = document.querySelector("#title")
-    if (h1 != null ) h1.textContent = name;
+  const h1 = document.querySelector("#title");
+  if (h1 != null) h1.textContent = name;
 }
 
 function makeDescription(description) {
-    let p = document.querySelector('#description')  
-    if (p != null ) p.textContent = description
+  const p = document.querySelector("#description");
+  if (p != null) p.textContent = description;
 }
 
-function makePrice(euro) {
-    let span = document.querySelector('#price')
-    if (span != null ) span.textContent = euro
+function makePrice(price) {
+  const span = document.querySelector("#price");
+  if (span != null) span.textContent = price;
 }
 
-function makeColors(colors) {
-    let select = document.querySelector("#colors")
+function makeOptions(colors) {
+  const select = document.querySelector("#colors");
+  if (select != null) {
+    colors.forEach((color) => {
+      const option = document.createElement("option");
+      option.value = color;
+      option.textContent = color;
+      select.appendChild(option);
+    });
+  }
 }
 
-function makeOptions() {
-    
+const button = document.querySelector("#addToCart");
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  const color = document.querySelector("#colors").value;
+  const quantity = document.querySelector("#quantity").value;
+
+  if (isOrderInvalid(color, quantity)) return;
+  saveOrder(color, quantity);
+
+  window.location.href = "cart.html";
+}
+
+// On sauvegarde les articles dans le localStorage
+
+function saveOrder(color, quantity) {
+  const data = {
+    id: id,
+    color: color,
+    quantity: Number(quantity),
+    price: itemPrice,
+    imageUrl: imgUrl,
+    altTxt: altText,
+  };
+  localStorage.setItem(id, JSON.stringify(data));
+}
+
+// Empêche le changement de page si un ou pluisieurs éléments est null
+
+function isOrderInvalid(color, quantity) {
+  if (color == null || color === "" || quantity == null || quantity == 0) {
+    alert(" Merci de choisir une couleur et une quantitée");
+    return true;
+  }
 }
--- front/js/script.js
+++ front/js/script.js
@@ -3,65 +3,63 @@
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data)
-    return getArticles(data)
-  })
+    console.log(data);
+    return getArticles(data);
+  });
 
-  // On répartit les données de l'API dans le DOM
+// On répartit les données de l'API dans le DOM
 
 function getArticles(products) {
-  
   products.forEach((product) => {
-    let { _id, imageUrl, altTxt, name, description } = product
-    let link = productLink(_id)
-    let article = document.createElement("article")
-    let image = productImg(imageUrl, altTxt)
-    let h3 = productName(name)
-    let p = productInfo(description)
-  
-    appendElementsToArticle(article, [image, h3, p])
-    appendArticleToLink(link, article)
-  })
+    let { _id, imageUrl, altTxt, name, description } = product;
+    let link = productLink(_id);
+    let article = document.createElement("article");
+    let image = productImg(imageUrl, altTxt);
+    let h3 = productName(name);
+    let p = productInfo(description);
+
+    appendElementsToArticle(article, [image, h3, p]);
+    appendArticleToLink(link, article);
+  });
 }
-  
+
 function appendElementsToArticle(article, array) {
   array.forEach((item) => {
-    article.appendChild(item)
-  })
+    article.appendChild(item);
+  });
 }
 
 function productLink(id) {
-  let productLink = document.createElement("a")
-  productLink.href = "./product.html?id=" + id
-  return productLink
+  let productLink = document.createElement("a");
+  productLink.href = "./product.html?id=" + id;
+  return productLink;
 }
 
-
 function appendArticleToLink(link, article) {
-  let items = document.querySelector("#items")
+  let items = document.querySelector("#items");
   if (items != null) {
-    items.appendChild(link)
-    link.appendChild(article)
+    items.appendChild(link);
+    link.appendChild(article);
   }
 }
 
 function productImg(imageUrl, altTxt) {
-  let image = document.createElement("img")
-  image.src = imageUrl
-  image.alt = altTxt
-  return image
+  let image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  return image;
 }
 
 function productName(name) {
-  let h3 = document.createElement("h3")
-  h3.textContent = name
-  h3.classList.add("productName")
-  return h3
+  let h3 = document.createElement("h3");
+  h3.textContent = name;
+  h3.classList.add("productName");
+  return h3;
 }
 
 function productInfo(description) {
-  let p = document.createElement("p")
-  p.textContent = description
-  p.classList.add("productDescription")
-  return p
+  let p = document.createElement("p");
+  p.textContent = description;
+  p.classList.add("productDescription");
+  return p;
 }
